c372220941dcb8f26b0887a34ca150e8b256a608
dahea <dahea.an@gmail.com>
2018-01-18 13:44:11 -0500

    attach event handlers to tempo markers

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 
 
 class TempoList extends Component {
+
   render() {
+    const setBpm = this.props.setBpm;
     const allBPMs = [40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 63, 66, 69, 72, 76, 80, 84, 88, 92, 96, 100, 104, 108, 112, 116, 120, 126, 132, 138, 144, 152, 160, 168, 176, 184, 192, 200, 208];
 
     return (
       <div>
           {allBPMs.map(function(bpm, index){
-            return <Tempos bpm={bpm} />;
+            return <Tempos key={index} setBpm={setBpm} bpm={bpm} />;
           })}
       </div>
     )
