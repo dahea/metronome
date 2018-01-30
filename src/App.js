981a033179efc551906de509b9d926d9015edf03
Peter Margaritoff <peter@resy.com>
2018-01-30 12:21:04 -0500

    Adds some comments for CR

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ class TempoList extends Component {
     const setBpm = this.props.setBpm;
     const allBPMs = [40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 63, 66, 69, 72, 76, 80, 84, 88, 92, 96, 100, 104, 108, 112, 116, 120, 126, 132, 138, 144, 152, 160, 168, 176, 184, 192, 200, 208];
 
+    // Nit: formatting of components with many props should be done like:
+    // <Component
+    //    prop1={abc}
+    //    prop2={xyz}
+    //    ...
+    // />
+    // for legibility
     return (
       <div>
           {allBPMs.map(function(bpm, index){
@@ -42,6 +49,12 @@ class MetronomeSwitch extends Component {
 }
 
 class Metronome extends Component {
+  // if you don't need to call super functions look into declaring state like so:
+  // state = {
+  //   abc: '',
+  //   xyz: ''
+  // }
+  // more concise
 
   constructor(props){
     super(props);
@@ -52,6 +65,8 @@ class Metronome extends Component {
       clickPlayer: 0,
       clickAudio: new Audio('./click.mp3')
     };
+
+    // if you use () => {} syntaxt binding these is no longer necessary as arrow maintain scope of parent block
     this.runMetronome = this.runMetronome.bind(this);
     this.changeBpm = this.changeBpm.bind(this);
     this.playClick = this.playClick.bind(this);
@@ -65,20 +80,27 @@ class Metronome extends Component {
       isMetronomeOn: !prevState.isMetronomeOn
     }));
 
+    // Do what you did above (passing in prevState) to ensure you're doing the math
+    // on the latest state
     this.setState({clickInterval: (60 / this.state.currentBpm)*1000});
 
     if (!this.state.isMetronomeOn) {
       this.playClick();
-    } 
+    }
 
     if (this.state.isMetronomeOn) {
       console.log(this.state.clickPlayer);
       this.stopClick();
-    } 
+    }
   }
 
   playClick(){
-
+    // Nitpick (legibility):
+    // Destructure state and/or props at the beginning of functions to make it easier to read.
+    // e.g:
+    // const {currentBpm, clickInterval, clickAudio} = this.state;
+    // then you can remove all the `this.state.abc`'s from the function body.
+    // There is nothing wrong with what you did, this just cleans it up a little
     console.log('metronome is playing at '+ this.state.currentBpm + ' which is every '+this.state.clickInterval+' ms');
     this.setState((prevState) => {
 
@@ -111,7 +133,7 @@ class Metronome extends Component {
 
     if (!this.state.isMetronomeOn) {
       console.log('metronome is off ' + newBpm);
-    } 
+    }
 
     if (this.state.isMetronomeOn) {
       clearInterval(this.state.clickPlayer);
@@ -123,7 +145,7 @@ class Metronome extends Component {
           console.log('updated click');
         }, newClickRate)};
       });
-    } 
+    }
   }
 
   render() {
