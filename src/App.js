387e57318a9910d1f8bb67d0f95bafbb4e5c6a8b
dahea <dahea.an@gmail.com>
2018-03-22 17:26:16 -0400

    revamp playlist concept to track lsit for session

--- src/App.js
+++ src/App.js
@@ -5,109 +5,50 @@ import Metronome from './metronome.js';
 
 //--------------------------------------------------------------------------
 
-class Playlist extends Component {
+class PracticeSet extends Component {
 
   state = {
-    playlistTitle: 'My playlist',
-    playlistDesc: 'This is a desc of my playlist',
-    songlist: []
-  }
+    tracklist: []
+  };
 
-  // will delete itself and add songs
-  deletePlaylist = (e) => {
-    console.log('set up confirmation and then delete');
-  }
 
-  editPlaylist = (e) => {
-    console.log('edit playlist title only');
-  }
+  createTrack = (e) => {
 
-  addPlaylistItem = (e) => {
-
-    const playlistRef = firebase.database().ref('playlist');
-    const song = {
-      songTitle: 'song title',
-      songBpm: 'song bpm rate',
-      songTime: 'song duration in minutes',
-      songBars: 'song duration in bars',
-      songDesc: 'song desc, not required'
-    };
-
-    playlistRef.push(song);
-
+    var tracklistarray = this.state.tracklist.slice();
+    tracklistarray.push('song');
+    this.setState(prevState => ({
+      tracklist: tracklistarray
+    }));
+    console.log(this.state.tracklist);
   }
 
   render() {
-    return (
-      <div>
-        <div>{this.state.playlistTitle}</div>
-        <div>{this.state.playlistDesc}</div>
-      {/* add this later: display "empty playlist. please add a song" message as default message/if no songs in playlist */}
-      {/* it should actually display info from localstorage */}
-
-        <div>
-          {
-
-            // this.state.songlist.map((songlist, index) => 
-            // <div>{songlist} {index+1}</div>
-            //)
-
-          }
-        </div>
-      
-        <button onClick={this.deletePlaylist}>Delete playlist</button>
-        <button onClick={this.editPlaylist}>Edit playlist</button>
-        <button onClick={this.addPlaylistItem}>Add song</button>
-      </div>
-    );
-  }
-}
-
-class PlaylistCollection extends Component {
-
-  createPlaylist = (e) => {
-
-    const playlistCollection = firebase.database().ref('playlistCollection');
-    const playlist = {
-      playlistTitle: 'My playlist',
-      playlistDesc: 'This is a desc of my playlist',
-      songlist: {}
-    };
-
-    playlistCollection.push(playlist);
-
-  }
-
-  render() {
-    var playlists
-
-    if (!firebase.database().ref()) {
-      playlists = <Playlist />
-      console.log('playlists already exist');
-
-    } else {
-      playlists = 'You have no playlists. Please add one.';
-    }
 
     return (
       <div className="playlist-wrapper">
         <div className="playlistcollection-wrapper">
-          {playlists}
+
+        {this.state.tracklist.length ? (
+          this.state.tracklist.map(function(name, index){
+                    return <div key={index}>{name}</div>;
+                  })
+        ) : (
+          'Please add a track'
+        )}
+
         </div>
-        <button>Create new playlist</button>
+        <button onClick={this.createTrack}>Add practice track</button>
       </div>
     );
   }
 }
-
-
 
 class App extends Component {
   render() {
     return (
       <div>
         <Metronome />
-        <PlaylistCollection />
+        <PracticeSet />
       </div>
     );
   }
