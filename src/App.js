33b1e0f3c637c348b4a291659ced9a3be6cf8cba
dahea <dahea.an@gmail.com>
2018-03-27 12:01:57 -0400

    screw extra features. the metronome works

--- src/App.js
+++ src/App.js
@@ -1,54 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import firebase from './firebase.js';
 import Metronome from './metronome.js';
 
 //--------------------------------------------------------------------------
-
-class PracticeSet extends Component {
-
-  state = {
-    tracklist: []
-  };
-
-
-  createTrack = (e) => {
-
-    var tracklistarray = this.state.tracklist.slice();
-    tracklistarray.push('song');
-    this.setState(prevState => ({
-      tracklist: tracklistarray
-    }));
-    console.log(this.state.tracklist);
-  }
-
-  render() {
-
-    return (
-      <div className="playlist-wrapper">
-        <div className="playlistcollection-wrapper">
-
-        {this.state.tracklist.length ? (
-          this.state.tracklist.map(function(name, index){
-                    return <div key={index}>{name}</div>;
-                  })
-        ) : (
-          'Please add a track'
-        )}
-
-        </div>
-        <button onClick={this.createTrack}>Add practice track</button>
-      </div>
-    );
-  }
-}
 
 class App extends Component {
   render() {
     return (
       <div>
         <Metronome />
-        <PracticeSet />
       </div>
     );
   }
